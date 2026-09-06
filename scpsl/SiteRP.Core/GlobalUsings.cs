cf980790f6c2a4fcce62bb0336414324843782e0
nexox9official-source <nexox9official@gmail.com>
2026-09-06 02:33:10 +0200

    chore: add global logger alias

--- scpsl/SiteRP.Core/GlobalUsings.cs
+++ scpsl/SiteRP.Core/GlobalUsings.cs
@@ -3,3 +3,4 @@ global using System.Collections.Generic;
 global using System.IO;
 global using System.Linq;
 global using LabApi.Features.Wrappers;
+global using Logger = LabApi.Features.Console.Logger;
